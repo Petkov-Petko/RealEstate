044f4bfc7fb90b57da3cb6698516adf1072ba74c
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-20 21:48:49 +0300

    Add Home, Login, PublicView, and Register pages each page component takes other components from the components folder and dosnt contain other functionality

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/PublicView/PublicView.tsx
@@ -0,0 +1,12 @@
+import NavBarPublic from "../../components/PublicPage/NavBarPublic/NavBarPublic";
+import "./PublicView.css";
+
+const PublicView = () => {
+  return (
+    <div>
+      <NavBarPublic />
+    </div>
+  );
+};
+
+export default PublicView;
