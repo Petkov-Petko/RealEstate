93696cae05e340f589a1e06abd558a19c023b15c
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-21 11:16:13 +0300

    Added the routes from the router component
    App component includes user authentication and routing. It adds the necessary imports for react-router-dom and the RouterProvider component. It also wraps the component with the UserAuthProvider component for user authentication.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { UserAuthProvider } from "./context/userAuthContext";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return(
+  <UserAuthProvider>
+    <RouterProvider router={router} />
+  </UserAuthProvider>
+  );
 }
 
-export default App
+export default App;
