81b2accbe13bb24694adf034f2b3473b8a4e902b
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-21 20:48:52 +0300

    chore: Add AboutUs component with CSS styling

---
+++ src/components/PublicPage/AboutUs/AboutUs.tsx
@@ -0,0 +1,12 @@
+import "./AboutUs.css"
+
+const AboutUs = () => {
+  return (
+    <div className="flex flex-col mt-7 items-center mx-5">
+      <h1 className="text-5xl">About Us</h1>
+      <p className="text-center pt-3 text-lg max-w-4xl">Welcome to <span className="span-color">Estato</span>, Varna's top real estate destination. Our expert team is here to help you buy, sell, or invest in the perfect property. Trust us to make your real estate dreams come true in this beautiful coastal city.</p>
+    </div>
+  )
+}
+
+export default AboutUs
