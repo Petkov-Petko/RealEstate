5e0fd5b4be6e67b1447a970528171343da2a6bb2
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-21 20:49:58 +0300

    feat: Add TitlePublic and AboutUs components to PublicView page

--- src/pages/PublicView/PublicView.tsx
+++ src/pages/PublicView/PublicView.tsx
@@ -1,10 +1,14 @@
 import NavBarPublic from "../../components/PublicPage/NavBarPublic/NavBarPublic";
 import "./PublicView.css";
+import TitlePublic from "../../components/PublicPage/TitlePublic/TitlePublic";
+import AboutUs from "../../components/PublicPage/AboutUs/AboutUs";
 
 const PublicView = () => {
   return (
     <div>
       <NavBarPublic />
+      <TitlePublic />
+      <AboutUs />
     </div>
   );
 };
