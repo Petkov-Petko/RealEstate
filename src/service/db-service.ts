634be51c9dddea8fc9a9cf59ad5474cc35634f8a
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-21 13:55:14 +0300

    feat: Add createUser and checkIfUserExists functions
    The `createUser` function allows for the creation of a new user in the database, while the `checkIfUserExists` function checks if a user with a given username or email already exists in the database.

---
+++ src/service/db-service.ts
@@ -0,0 +1,28 @@
+import {database} from "../config/firebase-config";
+import { ref, get, set, update, query, equalTo, orderByChild, push } from "firebase/database";
+
+
+interface UserDetails {
+  username: string;
+  email: string;
+}
+
+export const createUser = async (userDetails: UserDetails) => {
+  try {
+    return await set(ref(database, `users/${userDetails.username}`), userDetails);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const checkIfUserExists = async (username: string, email:string) => {
+  try {
+    const snapshot1 = await get(query(ref(database, "users"), orderByChild("username"), equalTo(username)));
+    const snapshot2 = await get(query(ref(database, "users"), orderByChild("email"), equalTo(email)));
+
+    return [snapshot1, snapshot2];
+  } catch (error) {
+    console.log(error);
+    return [null, null];
+  }
+}
