64d94c650c1caaca5d05af7b62f56d90481715dd
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-23 18:32:13 +0300

    Added empty sell component

---
+++ src/pages/Sell/Sell.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/NavBar/NavBar"
+
+const Sell = () => {
+  return (
+    <div>
+        <NavBar />
+        <h1>Sell</h1>
+    </div>
+  )
+}
+
+export default Sell
