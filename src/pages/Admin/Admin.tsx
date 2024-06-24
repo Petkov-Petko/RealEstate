85ecb5dc38dded1c6ba83d74c3b1a53049eb2721
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-24 19:07:29 +0300

    feat: Add Admin page with radio inputs for selecting options(Users, Properties and Add Property)

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,55 @@
+import "./Admin.css";
+import AddProperty from "../../components/AdminOptions/AddProperty/AddProperty";
+import NavBar from "../../components/NavBar/NavBar";
+import { useState } from "react";
+
+const Admin = () => {
+  const [selectedOption, setSelectedOption] = useState("users");
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
+ 
+  return (
+    <div>
+      <NavBar />
+      <div className="radio-inputs">
+        <label className="radio">
+          <input
+            type="radio"
+            name="radio"
+            value="users"
+            onChange={handleOptionChange}
+            checked={selectedOption === "users"}
+          />
+          <span className="name">Users</span>
+        </label>
+        <label className="radio">
+          <input
+            type="radio"
+            name="radio"
+            value="properties"
+            onChange={handleOptionChange}
+            checked={selectedOption === "properties"}
+          />
+          <span className="name">Properties</span>
+        </label>
+        <label className="radio">
+          <input
+            type="radio"
+            name="radio"
+            value="addProperty"
+            onChange={handleOptionChange}
+            checked={selectedOption === "addProperty"}
+          />
+          <span className="name">Add Property</span>
+        </label>
+      </div>
+      {selectedOption === "users" && <div>Users</div>}
+      {selectedOption === "properties" && <div>Properties</div>}
+      {selectedOption === "addProperty" && <AddProperty />}
+    </div>
+  );
+};
+
+export default Admin;
