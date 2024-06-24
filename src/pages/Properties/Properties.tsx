7c9544707a65bf914639e31f583261bfa85d160a
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-24 22:32:32 +0300

    feat: Add Properties page with NavBar and PropertiesForm components

---
+++ src/pages/Properties/Properties.tsx
@@ -0,0 +1,14 @@
+import "./Properties.css"
+import NavBar from "../../components/NavBar/NavBar"
+import PropertiesForm from "../../components/PropertiesForm/PropertiesForm"
+
+const Properties = () => {
+  return (
+    <div>
+        <NavBar />
+        <PropertiesForm />
+    </div>
+  )
+}
+
+export default Properties
