4c7a3dc9b07b5f2575299cb393b16ec835b62357
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-24 19:14:57 +0300

    feat: Add Property interface to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,3 +8,17 @@ export interface UserLogIn {
     email: string;
     password: string;
 }
+
+export interface Property {
+    name: string;
+    square: number | null;
+    price: number | null;
+    rooms: number | null;
+    type: string;
+    baths: number | null;
+    city: string;
+    street: string;
+    description: string;
+    deal: string;
+    photos: string[];
+}
