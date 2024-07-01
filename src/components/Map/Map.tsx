4c82faed76210467f43bff8f61347beb9d6ee8e1
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-01 15:13:34 +0300

    added markers to the map with an icon on the properties coordinates

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,10 @@
 import "./Map.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { Property } from "../../types/types";
-import axios from "axios";
+
 // import { Marker } from "@vis.gl/react-google-maps";
+import Markers from "./Markers";
 
 
 interface MapProps {
@@ -12,37 +13,33 @@ interface MapProps {
 }
 
 const Map = (props: MapProps) => {
-  const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([]);
+  // const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([]);
+console.log(props.properties);
 
 
-  useEffect(() => {
-    const fetchCoordinates = async () => {
+  // useEffect(() => {
+  //   const fetchCoordinates = async () => {
         
-      const promises = props.properties.map(property =>
-        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(property.street)} ${property.city}&key=${import.meta.env.VITE_GOOGLE_KEY}`)
-      );
-      const results = await Promise.all(promises);
-      const newMarkers = results
-        .filter(result => result.data.status === "OK" && result.data.results.length > 0) 
-        .map(result => result.data.results[0].geometry.location);
+  //     const promises = props.properties.map(property =>
+  //       axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(property.street)} ${property.city}&key=${import.meta.env.VITE_GOOGLE_KEY}`)
+  //     );
+  //     const results = await Promise.all(promises);
+  //     const newMarkers = results
+  //       .filter(result => result.data.status === "OK" && result.data.results.length > 0) 
+  //       .map(result => result.data.results[0].geometry.location);
       
   
-      setMarkers(newMarkers);
-    };
+  //     setMarkers(newMarkers);
+  //   };
   
-    fetchCoordinates();
-  }, [props.properties]);
+  //   fetchCoordinates();
+  // }, [props.properties]);
 
   
   return (
     <div className="mapContainerStyle">
       <GoogleMapReact
         bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_KEY}}
-        defaultCenter={
-          props.coordinates?.lat && props.coordinates?.lng
-            ? props.coordinates
-            : undefined
-        }
         center={
           props.coordinates?.lat && props.coordinates?.lng
             ? props.coordinates
@@ -54,8 +51,14 @@ const Map = (props: MapProps) => {
         // onChange={""}
         // onChildClick={""}
       >
+
+        {props.properties?.map((place, i)=> (
+          <div lat={Number(place.lat)} lng={Number(place.lng)} key={i}>
+            ✅
+          </div>
+        ))}
         
-         
+        {/* <Markers properties={props.properties}/> */}
    
       </GoogleMapReact>
     </div>
