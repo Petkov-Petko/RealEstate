803623274cd4fdc07f6d6eacce63a9f73ce537d9
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-02 22:29:24 +0300

    feat: Update map component to use props.coordinates for city filtering
    Added Markers component to the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,29 +1,37 @@
 import "./Map.css";
-
-import { Property } from "../../types/types";
 import {
   APIProvider,
   Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow
-} from "@vis.gl/react-google-maps"
-
+} from "@vis.gl/react-google-maps";
+import Markers from "./Markers";
+import { useEffect, useState } from "react";
 interface MapProps {
   coordinates: { lat: number; lng: number } | null;
-  properties: Property[];
 }
 
 const GoogleMap = (props: MapProps) => {
-  const position = { lat: 43.2141, lng: 27.9147 };
+  const [cityCoordinates, setCityCoordinates] = useState({ lat: 43.2141, lng: 27.9147 });
+
+  useEffect(() => {
+    if (props.coordinates) {
+      setCityCoordinates(props.coordinates);
+    }
+    // console.log("props.coordinates", props.coordinates);
+    // console.log("cityCoordinates", cityCoordinates);
+    
+  }, [props.coordinates]);
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_KEY}>
       <div className="mapContainerStyle">
-        <Map defaultZoom={12} defaultCenter={position} mapId={import.meta.env.VITE_GOOGLE_ID}>
-           <AdvancedMarker position={position}>
-            
-           </AdvancedMarker>
+        <Map
+          defaultZoom={12}
+          //! To fix changing the city on the map depending on filters.
+          // center={cityCoordinates} 
+          defaultCenter={cityCoordinates}
+          mapId={import.meta.env.VITE_GOOGLE_ID}
+        >
+          <Markers />
         </Map>
       </div>
     </APIProvider>
@@ -31,4 +39,4 @@ const GoogleMap = (props: MapProps) => {
 };
 
 export default GoogleMap;
-``
+``;
