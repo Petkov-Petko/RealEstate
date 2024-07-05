a0fad50ac72af18890b5e378ae12e372b91916f0
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-05 22:47:05 +0300

    added image to home view

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import home from './home.jpg';
 import loginImage from './loginImage.jpg';
+import home_top from './home_top.jpg';
 
 export const assets = {
     logo,
     home,
-    loginImage
+    loginImage,
+    home_top
 }
