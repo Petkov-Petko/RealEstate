d7896fc675d979928b97660630d39370b4817cba
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-13 14:45:38 +0300

    Update SingleProperty page layout and add property details
    This commit updates the layout of the SingleProperty page, including the property photos section and a photo gallery with the ability to view all property photos. Additionally, it adds the property details section, displaying the property name, price, location, and other details. The Property interface in types.ts was also refactored to include an id property.

--- src/pages/SingleProperty/SingleProperty.tsx
+++ src/pages/SingleProperty/SingleProperty.tsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import { getProperty } from "../../service/db-service";
 import { useState, useEffect } from "react";
+import { Property } from "../../types/types";
 
 const SingleProperty = () => {
   const { id = "" } = useParams();
-  const [property, setProperty] = useState(null);
+  const [property, setProperty] = useState<Property | null>(null);
   const [photos, setPhotos] = useState([]);
   const [showAllPhotos, setShowAllPhotos] = useState(false);
 
@@ -30,7 +31,7 @@ const SingleProperty = () => {
     <div>
       <NavBar />
       <div className="single_property">
-        <div className="single_property_photos">
+        <div className="flex gap-3 mt-3 items-center justify-center">
           <div className="left_photo">
             <img src={photos[0]} alt="property" />
           </div>
@@ -45,7 +46,12 @@ const SingleProperty = () => {
         </div>
         {showAllPhotos && (
           <div className="single_property_all_photos_container">
-            <div onClick={()=>setShowAllPhotos(false)} className="flex justify-end mr-10 text-2xl mt-3 cursor-pointer"><p>X</p></div>
+            <div
+              onClick={() => setShowAllPhotos(false)}
+              className="flex justify-end mr-10 text-2xl mt-3 cursor-pointer"
+            >
+              <p>X</p>
+            </div>
             <div className="single_property_all_photos">
               {photos.map((photo, index) => {
                 return <img key={index} src={photo} alt="property" />;
@@ -53,6 +59,52 @@ const SingleProperty = () => {
             </div>
           </div>
         )}
+        <div className="flex flex-col mt-3 gap-5">
+          <div className="flex justify-between items-center">
+            <h1 className="text-4xl">{property?.name}</h1>
+            <p className="text-2xl">
+              <span id="single_property_price">${property?.price}</span>
+              <span className="opacity-60">
+                {" "}
+                {property?.deal === "rent" ? "/ month" : "/ buy"}
+              </span>
+            </p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p>
+              <i className="fa-solid fa-location-dot fa-lg pr-1"></i>
+              {property?.street}, {property?.city}
+            </p>
+            <div>
+              <i className="fa-regular fa-star fa-lg pr-3"></i>
+              <i className="fa-regular fa-bookmark fa-lg"></i>
+            </div>
+          </div>
+          <div>
+            <ul className="flex gap-7 align-middle justify-center single_property_ul">
+              <li>
+                <i className="fa-solid fa-expand fa-lg pr-1"></i>
+                {property?.square}m2
+              </li>
+              <li>
+                <i className="fa-solid fa-door-open fa-lg pr-1"></i>
+                {property?.rooms} Rooms
+              </li>
+              <li>
+                <i className="fa-solid fa-bath fa-lg pr-1"></i>
+                {property?.baths} Baths
+              </li>
+              <li>
+                <i className="fa-solid fa-building fa-lg pr-1"></i>
+                {property?.type}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="text-2xl  pb-2">Properties details</p>
+            <p className="opacity-80 text-lg">{property?.description}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
