b071ff1ca48c90d42e906e963b912ad300e349d7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-15 16:51:51 +0300

    Added Footer component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { UserAuthProvider } from "./context/userAuthContext";
+import Footer from "./pages/Footer/Footer";
 
 function App() {
-  return(
-  <UserAuthProvider>
-    <RouterProvider router={router} />
-  </UserAuthProvider>
+  return (
+    <UserAuthProvider>
+      <RouterProvider router={router} />
+      <Footer />
+    </UserAuthProvider>
   );
 }
 
