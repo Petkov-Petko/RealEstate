ada798adb36555d030b004c6774f5ccddf29498d
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-15 16:54:10 +0300

    The Contact Us link was changed from "/contact" to "/contacts" to match the correct route.

--- src/components/PublicPage/NavBarPublic/NavBarPublic.tsx
+++ src/components/PublicPage/NavBarPublic/NavBarPublic.tsx
@@ -5,20 +5,16 @@ const NavBarPublic = () => {
   return (
     <div className="flex justify-between items-center mx-6 my-4">
       <div>
-        <img
-        className="w-30 h-16"
-          src={assets.logo}
-          alt="logo"
-        />
+        <img className="w-30 h-16" src={assets.logo} alt="logo" />
       </div>
       <div className="public-li-el">
         <ul className="flex flex-row gap-10 text-lg">
           <li>
             <a href="/about">About</a>
           </li>
-            <li>
-                <a href="/contact">Contact Us</a>
-            </li>
+          <li>
+            <a href="/contacts">Contact Us</a>
+          </li>
           <li>
             <a href="/login">Login</a>
           </li>
