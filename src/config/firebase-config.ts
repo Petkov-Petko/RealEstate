88165d819fd685afa2d939dba7c57c0a303a5502
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-17 23:26:19 +0300

    removed comment code

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -5,11 +5,6 @@ import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "realestate-dcdcb.firebaseapp.com",
