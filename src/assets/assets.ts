0a44a6b0c7afcda3c795e540906eaf912f3bc347
Petko Petkov <petkozlatilov1234@gmail.com>
2024-07-25 20:47:44 +0300

    added photos for the about_us component

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,10 +2,20 @@ import logo from './logo.png';
 import home from './home.jpg';
 import loginImage from './loginImage.jpg';
 import home_top from './home_top.jpg';
+import about_us_1 from './about_us_1.jpg';
+import about_us_2 from './about_us_2.jpg';
+import customer1 from './customer1.jpg';
+import customer2 from './customer2.jpg';
+import customer3 from './customer3.jpg';
 
 export const assets = {
     logo,
     home,
     loginImage,
-    home_top
+    home_top,
+    about_us_1,
+    about_us_2,
+    customer1,
+    customer2,
+    customer3,
 }
