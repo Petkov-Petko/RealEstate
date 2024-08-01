8f751f4e8cc0b8392ce0292db17a46988e0e0208
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-01 12:15:39 +0300

    Update Map Markers component to fetch and display property data

--- src/components/Map/Markers.tsx
+++ src/components/Map/Markers.tsx
@@ -1,46 +1,77 @@
 import { AdvancedMarker } from "@vis.gl/react-google-maps";
 import { getAllProperties } from "../../service/db-service";
 import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 
 type Prop = {
-    key: string;
-    name: string;
-    lat: number;
-    lng: number;
-  };
+  key: string;
+  name: string;
+  lat: number;
+  lng: number;
+  image: string;
+  type: string;
+  price: number;
+  deal: string;
+  likes: number;
+  id: string;
+};
 
 const Markers = () => {
-    const [properties, setProperties] = useState<Prop[]>([]);
+  const [properties, setProperties] = useState<Prop[]>([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const snapshot = await getAllProperties();
-            if(snapshot){
-                const properties = snapshot.val();
-                if (properties) {
-                    
-                    const formattedProperties = Object.values(properties).map((prop: any) => ({
-                        name: prop.name,
-                        lat: prop.lat,
-                        lng: prop.lng,
-                        key: JSON.stringify({ name: prop.name, lat: prop.lat, lng: prop.lng }),
-                    }));
-                    setProperties(formattedProperties);   
-                    console.log("formattedProperties", formattedProperties);
-                          
-                }
-            }
-           
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const snapshot = await getAllProperties();
+      if (snapshot) {
+        const properties = snapshot.val();
+        if (properties) {
+          console.log("properties", properties);
+
+          const formattedProperties = Object.values(properties).map(
+            (prop: any) => ({
+              id: prop.id,
+              image: prop.photos[0],
+              type: prop.type,
+              price: prop.price,
+              deal: prop.deal,
+              name: prop.name,
+              lat: prop.lat,
+              lng: prop.lng,
+              likes: prop.likes?.length ?? 0,
+              key: JSON.stringify({
+                name: prop.name,
+                lat: prop.lat,
+                lng: prop.lng,
+              }),
+            })
+          );
+          setProperties(formattedProperties);
+          console.log("formattedProperties", formattedProperties);
+        }
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       {properties.map((point, i) => (
         <AdvancedMarker position={point} key={i}>
-          <span>✅</span>
+          <div  style={{ pointerEvents: 'auto' }} onClick={()=> navigate(`/properties/${point.id}`)} className="marker_property flex cursor-pointer">
+            <div>
+              <img src={point.image} alt={point.name} />
+            </div>
+            <div className="marker_property_text pl-1 max-w-44 max-h-20 overflow-auto">
+              <h1 className="text-lg">{point.name}</h1>
+              <p className="font-bold">
+                ${point.price} {point?.deal === "rent" ? "/ month" : "/ buy"}
+              </p>
+              <p className="font-medium mt-1">
+                {point.type} <i className="fa-brands fa-gratipay fa-lg pr-1"></i> {point?.likes}
+              </p>
+            </div>
+          </div>
         </AdvancedMarker>
       ))}
     </>
