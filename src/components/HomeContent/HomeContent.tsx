d90c62e50bdb5f8a110c0c72371904bbea96cb9f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-01 09:40:48 +0300

    Added navigation when click on top property

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -4,11 +4,15 @@ import { getAllProperties } from "../../service/db-service";
 import { useEffect, useState } from "react";
 import { Property } from "../../types/types";
 import PropertyStyle from "../Property/Property";
+import { useNavigate } from "react-router-dom";
 
 const HomeContent = () => {
   const [properties, setProperties] = useState<Property[]>([]);
   const [popularProperty, setPopularProperty] = useState<Property | null>(null);
-  const [popularPropertyPhoto, setPopularPropertyPhoto] = useState<string | null>(null);
+  const [popularPropertyPhoto, setPopularPropertyPhoto] = useState<
+    string | null
+  >(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProperties = async () => {
@@ -30,28 +34,33 @@ const HomeContent = () => {
     };
 
     fetchProperties();
-    console.log(popularProperty);
-    
   }, []);
 
   const nextPhoto = () => {
-    const currentPhotoIndex = popularProperty?.photos.indexOf(popularPropertyPhoto ?? "");
+    const currentPhotoIndex = popularProperty?.photos.indexOf(
+      popularPropertyPhoto ?? ""
+    );
     let nextPhotoIndex;
-    if(popularProperty && currentPhotoIndex === popularProperty?.photos.length - 1){
-        nextPhotoIndex = 0;
-    }else{
-        nextPhotoIndex = currentPhotoIndex ? currentPhotoIndex + 1 : 1;
+    if (
+      popularProperty &&
+      currentPhotoIndex === popularProperty?.photos.length - 1
+    ) {
+      nextPhotoIndex = 0;
+    } else {
+      nextPhotoIndex = currentPhotoIndex ? currentPhotoIndex + 1 : 1;
     }
     setPopularPropertyPhoto(popularProperty?.photos[nextPhotoIndex] ?? null);
   };
 
   const prevPhoto = () => {
-    const currentPhotoIndex = popularProperty?.photos.indexOf(popularPropertyPhoto ?? "");
+    const currentPhotoIndex = popularProperty?.photos.indexOf(
+      popularPropertyPhoto ?? ""
+    );
     let prevPhotoIndex;
-    if(currentPhotoIndex === 0) {
-        prevPhotoIndex = (popularProperty?.photos.length ?? 0) - 1;
-    }else{
-        prevPhotoIndex = currentPhotoIndex ? currentPhotoIndex - 1 : 1;
+    if (currentPhotoIndex === 0) {
+      prevPhotoIndex = (popularProperty?.photos.length ?? 0) - 1;
+    } else {
+      prevPhotoIndex = currentPhotoIndex ? currentPhotoIndex - 1 : 1;
     }
     setPopularPropertyPhoto(popularProperty?.photos[prevPhotoIndex] ?? null);
   };
@@ -63,7 +72,7 @@ const HomeContent = () => {
       </div>
       <div className="home_title">
         <h1>FIND YOUR DREAM</h1>
-        <p className="w-96 text-xl">
+        <p className="w-80 text-xl">
           We provide a complete service of the sale, purchase or rental of real
           estate.
         </p>
@@ -73,31 +82,71 @@ const HomeContent = () => {
         <h1 className="text-center text-4xl font-black mt-11">Top Pick</h1>
         <div className="home_property">
           <div className="relative">
-            <img src={popularPropertyPhoto ?? popularProperty?.photos[0]} alt="first property" />
+            <img
+              src={popularPropertyPhoto ?? popularProperty?.photos[0]}
+              alt="first property"
+            />
             <div className="arrows">
-              <i onClick={prevPhoto} className="fa-solid fa-chevron-left fa-lg"></i>
-              <i onClick={nextPhoto} className="fa-solid fa-chevron-right fa-lg"></i>
+              <i
+                onClick={prevPhoto}
+                className="fa-solid fa-chevron-left fa-lg"
+              ></i>
+              <i
+                onClick={nextPhoto}
+                className="fa-solid fa-chevron-right fa-lg"
+              ></i>
             </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <h3>{popularProperty?.name}</h3>
-            <p className="font-bold text-lg">{popularProperty?.price}</p>
-            <p>{popularProperty?.street}</p>
-            <div className="flex gap-2">
-              <p>{popularProperty?.square} m2</p>
-              <p>
-                <i className="fa-solid fa-door-open fa-lg pr-1"></i>{popularProperty?.rooms} Rooms
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between items-center mt-2 ">
+              <p className="bg-teal-600 text-white p-1 rounded-xl text-lg">
+                {popularProperty?.type === "house" ? (
+                  <i className="fa-solid fa-house fa-sm pr-1"></i>
+                ) : (
+                  <i className="fa-solid fa-building fa-sm pr-1"></i>
+                )}
+                {popularProperty?.type}
               </p>
-              <p>
-                <i className="fa-solid fa-bath fa-lg pr-1"></i>{popularProperty?.baths} Bathrooms
+              <p>stars</p>
+            </div>
+            <div className="flex justify-between items-center">
+              <p
+                onClick={() => navigate(`/properties/${popularProperty?.id}`)}
+                className="property-title"
+              >
+                {popularProperty?.name}
+              </p>
+              <p className="font-bold">
+                ${popularProperty?.price}{" "}
+                {popularProperty?.deal === "rent" ? "/ month" : "/ buy"}
               </p>
             </div>
-            <p>{popularProperty?.description}</p>
+            <div className="flex justify-between items-center">
+              <p>{popularProperty?.city}</p>
+              <p>
+                <i className="fa-solid fa-location-dot fa-lg pr-1"></i>
+                {popularProperty?.street}
+              </p>
+            </div>
+            <div className="flex gap-3 items-center mt-1">
+              <p>
+                <i className="fa-solid fa-door-open fa-lg pr-1"></i>
+                {popularProperty?.rooms} Rooms
+              </p>
+              <p>
+                <i className="fa-solid fa-bath fa-lg pr-1"></i>
+                {popularProperty?.baths} baths
+              </p>
+              <p>
+                <i className="fa-solid fa-expand fa-lg pr-1"></i>
+                {popularProperty?.square}m2
+              </p>
+            </div>
           </div>
         </div>
         <div>
           <h1 className="text-center text-4xl font-black mt-11">Popular</h1>
-          <div className="popular-properties">
+          <div className="popular-properties mb-10">
             {properties.slice(0, 6).map((property, index) => (
               <PropertyStyle property={property} key={index} />
             ))}
