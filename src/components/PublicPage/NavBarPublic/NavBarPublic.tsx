32a9a8c6e893b9d1e0d9f2a7c1ce90ac674de78f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-05 09:59:38 +0300

    added more responsiveness to the public home page

--- src/components/PublicPage/NavBarPublic/NavBarPublic.tsx
+++ src/components/PublicPage/NavBarPublic/NavBarPublic.tsx
@@ -3,12 +3,12 @@ import { assets } from "../../../assets/assets";
 
 const NavBarPublic = () => {
   return (
-    <div className="flex justify-between items-center mx-6 my-4">
+    <div className="public_nav flex justify-between items-center mx-6 my-4">
       <div>
         <img className="w-30 h-16" src={assets.logo} alt="logo" />
       </div>
       <div className="public-li-el">
-        <ul className="flex flex-row gap-10 text-lg">
+        <ul className="public_nav_el flex flex-row gap-10 text-lg">
           <li>
             <a href="/aboutUS">About</a>
           </li>
