d807e9c1919c2f0e178e5f6af6d0168feadf6e0c
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-06 12:08:02 +0300

    Add SavedProperties component
    The commit message suggests adding a new component called SavedProperties.

---
+++ src/components/SavedProperties/SavedProperties.tsx
@@ -0,0 +1,14 @@
+import "./SavedProperties.css"
+
+const SavedProperties = () => {
+  return (
+    <div className="min-h-screen">
+        <h1 className="text-center text-4xl">Saved Properties</h1>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SavedProperties
