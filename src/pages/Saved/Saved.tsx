6f8416f36defc7251c183386305880aab03e6cd5
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-06 11:02:15 +0300

    Add Saved page

---
+++ src/pages/Saved/Saved.tsx
@@ -0,0 +1,11 @@
+import SavedProperties from '../../components/SavedProperties/SavedProperties'
+
+const Saved = () => {
+  return (
+    <div>
+      <SavedProperties />
+    </div>
+  )
+}
+
+export default Saved
