f2ad6e3e5d957b8cbc7d0579eb82533793a1b777
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-12 11:06:55 +0300

    Refactor password validation to include uppercase letters in RegisterForm.tsx

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,3 @@
 export const username_min_length: number = 2;
 export const username_max_length: number = 50;
-export const regex_password = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+export const regex_password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[A-Z])[A-Za-z\d@$!%*?&]{6,}$/;
