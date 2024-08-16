3190071c623a42566e1b56d6d68dea58de47f9d4
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-16 08:41:28 +0300

    Added documentation

--- src/service/storage.ts
+++ src/service/storage.ts
@@ -1,6 +1,14 @@
 import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
 import { storage } from "../config/firebase-config.js";
 
+/**
+ * Uploads a file to the storage.
+ * 
+ * @param propertyId - The ID of the property.
+ * @param file - The file to be uploaded.
+ * @param photoName - The name of the photo.
+ * @returns A promise that resolves when the file is uploaded.
+ */
 export const uploadFile = async (
   propertyId: string,
   file: File,
@@ -10,11 +18,17 @@ export const uploadFile = async (
   await uploadBytes(imageNameRef, file);
 };
 
+/**
+ * Retrieves the list of files associated with a specific property.
+ * 
+ * @param propertyId - The ID of the property.
+ * @returns A promise that resolves to an array of strings representing the URLs of the files.
+ * @throws If there is an error fetching the photos.
+ */
 export const getFiles = async (propertyId: string): Promise<string[]> => {
   try {
     const photosRef = ref(storage, `properties/${propertyId}`);
     const listResult = await listAll(photosRef);
-    console.log(listResult);
     const urlPromises = listResult.items.map((itemRef) =>
       getDownloadURL(itemRef)
     );
@@ -26,6 +40,13 @@ export const getFiles = async (propertyId: string): Promise<string[]> => {
   }
 };
 
+/**
+ * Uploads a user photo to the storage.
+ * 
+ * @param userEmail - The email of the user.
+ * @param file - The file to be uploaded.
+ * @returns A promise that resolves when the upload is complete.
+ */
 export const uploadUserPhoto = async (
   userEmail: string,
   file: File,
@@ -34,6 +55,13 @@ export const uploadUserPhoto = async (
   await uploadBytes(imageNameRef, file);
 };
 
+/**
+ * Retrieves the URL of the user's photo from the storage.
+ * 
+ * @param userEmail - The email of the user.
+ * @returns A Promise that resolves to the URL of the user's photo.
+ * @throws If there is an error fetching the photo.
+ */
 export const getUserPhoto = async (userEmail: string): Promise<string> => {
   try {
     const photoRef = ref(storage, `users/${userEmail}/photo`);
