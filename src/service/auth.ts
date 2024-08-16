972f05ffc0ad9e04d289365aca0c548c866ad305
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-16 08:41:37 +0300

    Added documentation

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,46 +1,19 @@
-import {getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, deleteUser, signInWithPopup, updateProfile } from 'firebase/auth';
+import {getAuth, GoogleAuthProvider, signOut, signInWithPopup } from 'firebase/auth';
 import { auth } from '../config/firebase-config.js';
 
-export const signUp = async (emailAddress: string, password: string, displayName: string) => {
-    try {
-        const userCredentials = await createUserWithEmailAndPassword(auth, emailAddress, password);
-        if (userCredentials.user) {
-            await updateProfile(userCredentials.user, {
-                displayName: displayName,
-            });
-        }
-        return userCredentials;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        if(error.code === 'auth/email-already-in-use') {
-            alert("Email already exists");
-        }
-    }
-}
+/**
+ * Sign up a user with the provided email address, password, and display name.
+ * 
+ * @param emailAddress - The email address of the user.
+ * @param password - The password of the user.
+ * @param displayName - The display name of the user.
+ * @returns A promise that resolves to the user credentials.
+ */
 
-
-export const handleUserDelete = async () => {
-    try {
-        if (auth.currentUser) {
-            return await deleteUser(auth.currentUser);
-        } else {
-            throw new Error('User not found');
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-export const logIn = async (emailAddress: string, password: string) => {
-    try {
-        const userCredentials = await signInWithEmailAndPassword(auth, emailAddress, password);
-        return userCredentials;
-    } catch (error) {
-        throw new Error('Invalid email or password');
-    }
-}
-
+/**
+ * Logs out the user.
+ * @returns {Promise<void>} A promise that resolves when the user is successfully logged out.
+ */
 export const logOut = async () => {
     try {
         await signOut(auth);
@@ -49,6 +22,10 @@ export const logOut = async () => {
     }
 }
 
+/**
+ * Signs in the user using Google authentication.
+ * @returns An object containing the user's email and username if the sign-in is successful.
+ */
 export const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
