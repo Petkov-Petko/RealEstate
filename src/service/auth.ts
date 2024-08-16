9ba2ecb9d58d57d171f9c25775f1c16be442420b
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-16 08:50:24 +0300

    Returned the deleted signUp function

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,5 +1,6 @@
-import {getAuth, GoogleAuthProvider, signOut, signInWithPopup } from 'firebase/auth';
+import {getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, deleteUser, signInWithPopup, updateProfile } from 'firebase/auth';
 import { auth } from '../config/firebase-config.js';
+
 
 /**
  * Sign up a user with the provided email address, password, and display name.
@@ -9,6 +10,58 @@ import { auth } from '../config/firebase-config.js';
  * @param displayName - The display name of the user.
  * @returns A promise that resolves to the user credentials.
  */
+export const signUp = async (emailAddress: string, password: string, displayName: string) => {
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, emailAddress, password);
+        if (userCredentials.user) {
+            await updateProfile(userCredentials.user, {
+                displayName: displayName,
+            });
+        }
+        return userCredentials;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        if(error.code === 'auth/email-already-in-use') {
+            alert("Email already exists");
+        }
+    }
+}
+
+/**
+ * Handles the deletion of the current user.
+ * 
+ * @returns {Promise<void>} A promise that resolves when the user is deleted.
+ * @throws {Error} If the current user is not found.
+ */
+export const handleUserDelete = async () => {
+    try {
+        if (auth.currentUser) {
+            return await deleteUser(auth.currentUser);
+        } else {
+            throw new Error('User not found');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+/**
+ * Logs in a user with the provided email address and password.
+ * 
+ * @param emailAddress - The email address of the user.
+ * @param password - The password of the user.
+ * @returns A promise that resolves to the user credentials.
+ * @throws An error with the message 'Invalid email or password' if the login fails.
+ */
+export const logIn = async (emailAddress: string, password: string) => {
+    try {
+        const userCredentials = await signInWithEmailAndPassword(auth, emailAddress, password);
+        return userCredentials;
+    } catch (error) {
+        throw new Error('Invalid email or password');
+    }
+}
 
 /**
  * Logs out the user.
